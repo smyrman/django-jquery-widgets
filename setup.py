3b7affe5c8e423e00149ba3ad3312acf8c13f321
Sindre Røkenes Myren <smyrman@gmail.com>
2010-06-04 09:08:25 +0200

    Modified setup.py and added MANIFEST.in so that sdist works

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+VERSION = '0.1'
 
-setup(name='jQuery Widgets',
-      version='0.1',
-      description='jQuery-powered widgets and fields',
+setup(name='django-jquery-widgets',
+      version=VERSION,
+      description='jQuery-powered widgets and fields for Django',
       author='Sindre Røkenes Myren',
       author_email='smyrman@gmail.com',
       url='http://github.com/smyrman/django-jquery-widgets',
-      packages=['jquery_widgets', ],
-      include_package_data = True, # include everything in source control
+	  download_url='http://github.com/smyrman/django-jquery-widgets/tarball/%s'%VERSION,
+      packages=['jquery_widgets'],
+	  package_data={'jquery_widgets': ['media/js/*.js', 'media/css/*.css']},
+	  requires=['django (>=1.1)'],
 )

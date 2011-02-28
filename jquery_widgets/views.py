5699517c353a1b325c6199b7a99acd4b084f652e
Sindre Røkenes Myren <smyrman@gmail.com>
2011-02-28 05:46:16 +0100

    Converted to use JQuery-UI. Support for Django 1.3 staticfiles.

--- jquery_widgets/views.py
+++ jquery_widgets/views.py
@@ -9,6 +9,16 @@ from django.db import models
 from django.http import HttpResponse, HttpResponseNotFound
 from django.utils.encoding import smart_str
 
+def construct_search(field_name):
+	# use different lookup methods depending on the notation
+	if field_name.startswith('^'):
+		return "%s__istartswith" % field_name[1:]
+	elif field_name.startswith('='):
+		return "%s__iexact" % field_name[1:]
+	elif field_name.startswith('@'):
+		return "%s__search" % field_name[1:]
+	else:
+		return "%s__icontains" % field_name
 
 def fk_search(request, q=None):
 	"""Searches in the fields of the given related model and returns the
@@ -19,28 +29,23 @@ def fk_search(request, q=None):
 	app_label = request.GET.get('app_label', None)
 	model_name = request.GET.get('model_name', None)
 	search_fields = request.GET.get('search_fields', None)
-	if search_fields and app_label and model_name and query:
-		def construct_search(field_name):
-			# use different lookup methods depending on the notation
-			if field_name.startswith('^'):
-				return "%s__istartswith" % field_name[1:]
-			elif field_name.startswith('='):
-				return "%s__iexact" % field_name[1:]
-			elif field_name.startswith('@'):
-				return "%s__search" % field_name[1:]
-			else:
-				return "%s__icontains" % field_name
+	if not search_fields and app_label and model_name and query:
+		return HttpResponseNotFound()
 
-		model = models.get_model(app_label, model_name)
-		qs = model._default_manager.all()
-		for bit in query.split():
-			or_queries = [models.Q(**{construct_search(
-				smart_str(field_name)): smart_str(bit)})
-					for field_name in search_fields.split(',')]
-			other_qs = models.query.QuerySet(model)
-			other_qs.dup_select_related(qs)
-			other_qs = other_qs.filter(reduce(operator.or_, or_queries))
-			qs = qs & other_qs
-		data = ''.join([u'%s|%s\n' % (f.__unicode__(), f.pk) for f in qs])
-		return HttpResponse(data)
-	return HttpResponseNotFound()
+	fields = search_fields.split(',')
+	model = models.get_model(app_label, model_name)
+	qs = model._default_manager.all()
+	for bit in query.split():
+		or_queries = [models.Q(**{construct_search(smart_str(fn)):
+		              smart_str(bit)}) for fn in fields]
+		other_qs = models.query.QuerySet(model)
+		other_qs.dup_select_related(qs)
+		other_qs = other_qs.filter(reduce(operator.or_, or_queries))
+		qs = qs & other_qs
+	data = '{"objects":[%s]}' % ','.join(
+			('{"label":"%s","val":%s,"desc":"%s"}' %
+			(unicode(i), i.pk, " ".join((getattr(i, f) for f in fields)))
+			for i in qs))
+	response = HttpResponse(data)
+	response['Content-Type'] = 'text/json; charset=utf-8'
+	return response
--- jquery_widgets/widgets.py
+++ jquery_widgets/widgets.py
@@ -8,43 +8,20 @@ from django.conf import settings
 from django.utils.safestring import mark_safe
 from django.utils.text import truncate_words
 from django.template.loader import render_to_string
-
-# Get JQuery URL from settings, or fallback to default value:
-JS_BASE = tuple() # Basic tuple of javascripts to extend.
-try:
-	JQUERY_URL = settings.JQUERY_URL
-except AttributeError:
-	JQUERY_URL = 'js/jquery.min.js'
-
-	# Django 1.2 and later include a version of jQury in contrib.admin:
-	if VERSION[:2] >= (1, 2):
-		try:
-			JQUERY_URL = settings.ADMIN_MEDIA_PREFIX + JQUERY_URL
-			# Django admin keep it's jQuery in the 'django.jQuey' namespace,
-			# which is kind of anoying. If we are to use jQury extensions
-			# without modifications, we must make sure jQuery is put back into
-			# the global namespace:
-			JS_BASE = (
-				settings.ADMIN_MEDIA_PREFIX+'jquery.init.js',
-				'jquery_widgets/js/django.jquery.reinit.js',
-			)
-		except AttributeError:
-			JQUERY_URL = 'js/jquery.min.js'
-
-JS_BASE = (JQUERY_URL,) + JS_BASE
-
-
+from django.conf import settings
 
 class ForeignKeySearchInput(forms.HiddenInput):
 	""" A Widget for displaying ForeignKeys in an autocomplete search input
 	instead in a ``select`` box.
 	"""
 	class Media:
-		css = {
-			'all': ('jquery_widgets/css/jquery.autocomplete.css',)
+		css = {"all": (
+				settings.STATIC_URL+"css/ui-lightness/jquery-ui.custom.css",
+			),
 		}
-		js = JS_BASE + (
-			'jquery_widgets/js/jquery.autocomplete.min.js',
+		js = (
+			settings.STATIC_URL+"js/jquery.min.js",
+			settings.STATIC_URL+"js/jquery-ui.min.js",
 		)
 
 	def label_for_value(self, value):
